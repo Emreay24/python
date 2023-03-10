56afb2c77727a32075e0caa52ebc71da486a4ae9
Emreay24 <122545769+Emreay24@users.noreply.github.com>
2023-03-10 22:27:33 +0300

    ilk adımlar
    python hakkında ilk adımlar

---
+++ first.py
@@ -0,0 +1,16 @@
+isim="emre"
+yas = 23
+egitim_durumu= "lise"
+
+sinif_gereksinimleri= "lise" or "universite"
+
+if (yas>=18):
+    if egitim_durumu=="lise" or "universite":True
+
+    else:False
+if True:
+    print("sinifa girebilirsiniz")
+else:
+    print("sinifa giremezsiniz")
+
+
